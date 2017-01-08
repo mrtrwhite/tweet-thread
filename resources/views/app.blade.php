1843a3689ca87879114261e93a19873ffd557156
Tim White <twhite@thinkingjuice.co.uk>
2017-01-08 22:48:48 +0000

    Begin styling

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Tweet Chain</title>
+		<title>Tweet Thread</title>
 		<meta name="description" content="Create a tweet chain from a single block of text."/>
 		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 		<link rel="stylesheet" href="/css/style.min.css">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.1/knockout-min.js"></script>
 	</head>
 	<body>
+		<header class="header">
+			<h1>Tweet Thread</h1>
+		</header>
 		@yield('content')
+		<footer class="footer">
+			&#xa9;{{ date('Y') }} Tweet Thread was made by <a href="http://twitter.com/mrtrwhite">Tim White</a>
+		</footer>
 		<script src="/js/bundle.min.js"></script>
 	</body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,7 @@
 
 @section('content')
     <div class="home-wrap wrap height-auto">
-        <h1 class="title">Tweet Chain</h1>
-        <p class="intro">Write multiple tweets with a single block of text.</p>
+        <p class="intro">Chain tweets from a single block of text.</p>
         <a class="twitter-button js-authorise">Sign In</a>
     </div>
 @endsection
